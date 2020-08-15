9df12f9ac98c92b12580dc3c7c6a80d52530e6f5
andre-a21604285 <andrego21.04@gmail.com>
2020-08-15 20:26:19 +0100

    movement

--- src/pt/ulusofona/lp2/fandeisiaGame/Equipa.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/Equipa.java
@@ -6,14 +6,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Equipa {
-    List<Creature> creatures;
-    int id, moedas, pontos, size;
+    private int id, moedas, pontos, size;
 
 
     Equipa(int id){
         this.moedas = 50;
         this.id = id;
-        creatures = new ArrayList<Creature>();
         pontos=0;
         size=0;
     }
@@ -21,82 +19,25 @@ public class Equipa {
     public void addCreature(int id, String tipo, int x, int y, String orientacao){
         switch(tipo){
             case "Anao":
-                creatures.add( new Anao(id,this.id,x,y,orientacao));
                 setMoedas(Integer.parseInt(Anao.COST));
                 break;
             case "Dragao":
-                creatures.add( new Dragao(id,this.id,x,y,orientacao));
                 setMoedas(Integer.parseInt(Dragao.COST));
                 break;
             case "Humano":
-                creatures.add( new Humano(id,this.id,x,y,orientacao));
                 setMoedas(Integer.parseInt(Humano.COST));
                 break;
             case "Gigante":
-                creatures.add( new Gigante(id,this.id,x,y,orientacao));
                 setMoedas(Integer.parseInt(Gigante.COST));
                 break;
             case "Elfo":
-                creatures.add( new Elfo(id,this.id,x,y,orientacao));
                 setMoedas(Integer.parseInt(Elfo.COST));
                 break;
             case "Druida":
-                creatures.add( new Druida(id,this.id,x,y,orientacao));
                 setMoedas(Integer.parseInt(Druida.COST));
                 break;
         }
         size++;
-
-
-    }
-
-    public List<Tresure> movimento(int linhas, int colunas, Mapa map,List<Tresure> tresures){
-        for(int i = 0 ; i<creatures.size(); i++){
-            for(int j=0;j<creatures.get(i).getMovement();j++){
-                int x = creatures.get(i).getX() + position(creatures.get(i).getOrientation())[0];
-                int y = creatures.get(i).getY() + position(creatures.get(i).getOrientation())[1];
-                if(x<colunas && x>=0 && y<linhas && y>=0 ){
-                    if(j==creatures.get(i).getMovement()-1){ //TALVEZ APAGAR
-                        if(!checkMovement(x,y,creatures.get(i),map) || map.checkBuraco(x,y) || map.checkCreature(x,y)){
-                            creatures.get(i).setOrientation();
-                            break;
-                        }
-                        if(isDruidaInPar(creatures.get(i))) {
-                            tresures.add(new Tresure("bronze",creatures.get(i).getX(),creatures.get(i).getY()));
-                        }
-                        creatures.get(i).incKm();
-                        creatures.get(i).movimento();
-                        for(int z=0;z<tresures.size();z++){
-                            if(tresures.get(z).getX()==x && tresures.get(z).getY()==y){
-                                setPontos(tresures.get(z).getPoints(),creatures.get(i));
-                                tresures.remove(tresures.get(z));
-                            }
-                        }
-
-                    }else if(!checkMovement(x,y,creatures.get(i),map)){
-                        break;
-                    }else{
-                        if(isDruidaInPar(creatures.get(i))) {
-                            tresures.add(new Tresure("bronze",creatures.get(i).getX(),creatures.get(i).getY()));
-                            map.addPosition(creatures.get(i).getX(), creatures.get(i).getY(), 'b');
-                        }
-                        creatures.get(i).incKm();
-                        creatures.get(i).movimento();
-                        for(int z=0;z<tresures.size();z++){
-                            if(tresures.get(z).getX()==x && tresures.get(z).getY()==y){
-                                setPontos(tresures.get(z).getPoints(),creatures.get(i));
-                                tresures.remove(tresures.get(z));
-                            }
-                        }
-                    }
-                }else{
-                    creatures.get(i).setOrientation();
-                    break;
-                }
-
-            }
-        }
-        return tresures;
     }
 
     public int getMoedas(){
@@ -109,9 +50,6 @@ public class Equipa {
 
     public int getId(){return id;}
 
-    public List<Creature> getCreatures(){
-        Collections.sort(creatures, new Sort());
-        return creatures;}
 
     public int getPontos(){return pontos;}
 
@@ -123,7 +61,7 @@ public class Equipa {
     public int getSize(){return size;}
 
     //   Private Methods
-    private int[] position(String orientation){
+    public int[] position(String orientation){
         int[] position = new int[2];
         if(orientation.equals("Norte")){
             position[0] = 0;
@@ -153,7 +91,7 @@ public class Equipa {
         return position;
     }
 
-    private boolean checkMovement(int x, int y, Creature creature, Mapa map){
+    public boolean checkMovement(int x, int y, Creature creature, Mapa map){
         boolean isValid=false;
         if(creature.getTipo().equals("Elfo")){
                isValid = !map.checkCreature(x,y);
@@ -175,7 +113,7 @@ public class Equipa {
         return isValid;
     }
 
-    private boolean isDruidaInPar(Creature creature){
+    public boolean isDruidaInPar(Creature creature){
         if(creature.getTipo().equals(Druida.TIPO)&&(creature.getX()+creature.getY())%2==0){
             return true;
         }
